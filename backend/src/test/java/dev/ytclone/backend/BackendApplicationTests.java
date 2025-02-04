7d4a07b558eb242c3d732e8fc068ec77696230dc
Sven Laht <Sven.Laht@gmail.com>
2025-02-04 11:41:53 +0200

    Added backend and started work on docker

---
+++ backend/src/test/java/dev/ytclone/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ytclone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
